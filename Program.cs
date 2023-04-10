4757a91ba79bce395a52c29e8cbac462b1c834b3
Pavel Sadykov <2439747@gmail.com>
2023-04-10 18:14:19 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ToCelsius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("Usage: ToCelsius.exe <temperature in F>");
+                return;
+            }
+
+            double fahrenheit;
+            if (!double.TryParse(args[0], out fahrenheit))
+            {
+                Console.WriteLine("Invalid input: {0}", args[0]);
+                return;
+            }
+
+            double celsius = (fahrenheit - 32) * 5 / 9;
+
+            Console.WriteLine("{0} F = {1} C", fahrenheit, celsius);
+        }
+    }
+}
